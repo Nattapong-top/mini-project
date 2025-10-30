5c8bbd54a26ea097cb0c428b265a2074acea0d20
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2025-10-30 21:24:42 +0700

    สร้างไฟล บันทึก sqlite3เรียบร้อย

--- GUI/database.py
+++ GUI/database.py
@@ -7,4 +7,32 @@ DB_FILE = 'billing_records.db'
 
 def init_db():
     '''ฟังก์ชันสร้างไฟล์ฐานข้อมูล .db และสร้างตาราง (Table) ถ้ายังไม่มี '''
-    
+    try:
+        conn = sqlite3.connect(DB_FILE)
+        cursor = conn.cursor()
+
+        # สร้างตารางชื่อ 'invoices'
+        # เก็บข้อมูลสำคัญไว้ใน database 
+        cursor.execute("""
+        CREATE TABALE IF NOT EXISTS invoices (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tenant_name TEXT NOT NULL,
+            room_number TEXT,
+            invoice_date TEXT NOT NULL,
+            water_units INTEGER,
+            electric_units INTEGER,
+            water_cost REAL,
+            electric_cost REAL,
+            grand_total REAL NOT NULL
+        )
+        """)
+
+        conn.commit()
+        conn.close()
+        print("ฐานข้อมูล '{DB_FILE}' พร้อมใช้งาน")
+
+    except sqlite3.Error as e:
+        print(f'เกิดข้อผิดพลาดกับฐานข้อมูล: {e}')
+
+def 
+
