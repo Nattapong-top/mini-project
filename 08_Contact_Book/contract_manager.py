94d41331c3a5727c32a1ce6d62a2668cf530a799
Nattapong-top <Nattapong.top@hotmail.com>
2025-12-20 20:25:30 +0700

    08 Contract Book First Commit

---
+++ 08_Contact_Book/contract_manager.py
@@ -0,0 +1,11 @@
+'''Project 8: สมุดโทรศัพท์ (Contact Book)
+เป้าหมาย: สร้างสมุดโทรศัพท์ที่เก็บ ชื่อ, เบอร์โทร, อีเมล และแยกการทำงานเป็นสัดส่วน'''
+
+import os
+
+# 
+script_dir = os.path.dirname(__file__)
+filename = os.path.join(script_dir, 'contract.txt')
+
+
+
